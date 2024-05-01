a13c8a148a74974286d7cad369cc5d21b5551265
mtar0012 <mtar0012@student.monash.edu>
2024-05-01 16:27:24 +0800

    Added javadoc for methods & created an enum class for animals

---
+++ Project/Sprint2/src/main/game/Animal.java
@@ -0,0 +1,29 @@
+package main.game;
+
+/**
+ * Represents an animal enum for the volcano cards.
+ */
+public enum Animal {
+    BABY_DRAGON("babydragon.png"),
+    BAT("bat.png"),
+    SALAMANDER("salamander.png"),
+    SPIDER("spider.png");
+
+    private final String imageName;
+
+    /**
+     * Constructs an Animal enum with the specified image name.
+     * @param imageName The name of the image associated with the animal.
+     */
+    Animal(String imageName) {
+        this.imageName = imageName;
+    }
+
+    /**
+     * Gets the image name associated with the animal.
+     * @return The image name.
+     */
+    public String getImageName() {
+        return imageName;
+    }
+}
