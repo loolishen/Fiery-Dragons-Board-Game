c98236982a6a4d98043f90e04e8b7a1feb2e0421
lloo0011 <lloo0011@student.monash.edu>
2024-05-16 15:49:14 +1000

    This is essentially going to be the parent class for all the entity class factory

---
+++ demo/src/main/java/com/example/demo/EntityFactory/SpawnFactory.java
@@ -0,0 +1,21 @@
+package com.example.demo.EntityFactory;
+
+
+import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.entity.EntityFactory;
+import com.almasb.fxgl.entity.SpawnData;
+
+/**
+ * Abstract class for classes that use the FXGL entity factory interface. Its default spawn()
+ * method ensures that they register themselves to FXGL's game world as entity factories.
+ * It requires them to call super() by default, and allows them to override the spawn() method to add to the SpawnData
+ * payload.
+ */
+public abstract class SpawnFactory implements EntityFactory {
+    protected SpawnData spawnData;
+
+    public void spawn(){
+        FXGL.getGameWorld().addEntityFactory(this);
+        spawnData = new SpawnData();
+    }
+}
